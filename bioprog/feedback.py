41248daf211c41e7965e76df5e538bd2a5e85280
Pen Holland <pen.holland@york.ac.uk>
2020-07-21 16:59:12 +0100

    added rabbits feedback function

--- bioprog/feedback.py
+++ bioprog/feedback.py
@@ -8,3 +8,13 @@ def testFeedback(answ):
 		print('Incorrect: Your answer is not the correct string.')
 	return
 
+def rabbits(t, Nt):
+	N0 = 24
+	R = 9.97
+	Nans = N0 * R**t
+	if abs(Nt - Nans) <= 1:
+		print('Correct: 24 * 9.97 ^ {0} = {1}, which is within 1 of your answer {2}'.format(t, round(Nans, 2), Nt)
+	else:
+		print('Incorrect: try again.')
+	return
+	

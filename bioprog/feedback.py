987f084b2c668608de75827e999f9bb1c2b6a275
Pen Holland <pen.holland@york.ac.uk>
2020-08-05 16:06:25 +0100

    updated MCQ function

--- bioprog/feedback.py
+++ bioprog/feedback.py
@@ -27,25 +27,41 @@ def rabbits(t=None, Nt=None):
 		print('Incorrect: your answer ({0}) is too small.'.format(Nt))
 	return
 	
-def MCQ(q, a):
+def MCQ(q, A=None):
+	a = A.upper()
+
 	if q == 'testMCQ':
 		switcher={
 			'A': 'Correct',
+			'B': 'Incorrect: this is a single line comment in C',
+			'C': 'Incorrect: this is more commonly used for multiple line comments in C',
+			'D': 'Incorrect: this is a string'
+		}
+	elif q == 'sep':
+		switcher={
+			'A': 'Incorrect',
 			'B': 'Incorrect',
 			'C': 'Incorrect',
-			'D': 'Incorrect',
-			'x': '\'x\' is the default response - please update with A, B, C or D'
+			'D': 'Correct'
 		}
-		return switcher.get(a, 'Invalid answer: please check your response and try again')	
-		
-	if q == 'morse_list_comprehension':
+	elif q == 'Z':
+		switcher={
+			'A': 'Incorrect: B is the letter at position 1.',
+			'B': 'Correct: Z can be accessed by taking the first letter from the end of the alphabet.',
+			'C': 'Correct: Python counts from zero so Z is the 25th letter in the alphabet list.',
+			'D': 'Incorrect: Python counts from zero, not one, so there is no letter at position 26 in the alphabet list.'
+		}
+	elif q == 'morse_list_comprehension':
 		switcher={
 			'A': 'Incorrect',
 			'B': 'Incorrect',
 			'C': 'Correct',
-			'D': 'Incorrect',
-			'x': '\'x\' is the default response - please update with A, B, C or D'
+			'D': 'Incorrect'
 		}
-		return switcher.get(a, 'Invalid answer: please check your response and try again')	
 	else:
+	
 		return('Question name not recognised: please try again.')
+	
+	switcher['x'] = '\'x\' is the default response - please update with A, B, C or D'
+	switcher[None] = 'No response given - please update with A, B, C or D'
+	return switcher.get(a, 'Invalid answer: please check your response and try again')		
